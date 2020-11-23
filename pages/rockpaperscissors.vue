1434de2a31c0789101811e146c16fb59b5559eb0
Alex B <abailon949@gmail.com>
2020-11-22 20:20:58 -0800

    added animate css

--- pages/rockpaperscissors.vue
+++ pages/rockpaperscissors.vue
@@ -1,4 +1,5 @@
 <script>
+import 'animate.css';
 export default {
   data () {
     return {
@@ -18,14 +19,17 @@ export default {
       possibleChoices:[ 
         {
           name: 'Rock',
+          delay: '1',
           url: 'https://www.pinclipart.com/picdir/big/536-5360218_rock-paper-scissors-clipart-png-download-paper-rock.png'
         }, 
         {
           name: 'Paper',
+          delay: '2',
           url: 'https://www.pinclipart.com/picdir/big/536-5360310_transparent-rock-paper-scissors-clipart-rock-paper-scissors.png'
         },
         {
           name: 'Scissors',
+          delay: '3',
           url: 'https://www.pinclipart.com/picdir/big/536-5360227_scissors-hand-rock-paper-scissors-png-clipart.png'
         }
       ]   
@@ -51,33 +55,23 @@ export default {
       <v-row justify="center">
         <v-col v-for="choice in possibleChoices" :key="choice.name" cols="12" sm="6">
           <v-card flat @click="choiceMade(choice)" >
-            <v-img contain height="300" :src="choice.url"/>
+            <v-img :class="'animate__animated animate__tada animate__delay-'+ choice.delay +'s'" contain height="300" :src="choice.url"/>
           </v-card>
         </v-col>
       </v-row>
     </div> 
     <div v-else>
-      <v-row align="center">
-        <h1> {{ finalResults }} </h1> <v-spacer/> <v-btn @click="userChoice = null" color="primary">Play Again</v-btn>
+      <v-row align="center" justify="space-around">
+        <h1> {{ finalResults }} </h1> <v-btn @click="userChoice = null" color="primary">Play Again</v-btn>
       </v-row>
-      <v-row>
+      <v-row >
         <v-col cols="12" sm="6">
-          <h1>You selected</h1>
-          <v-card>
-            <v-img height="250" :src="userChoice.url"/>
-            <v-card-title>
-              {{ userChoice.name }}
-            </v-card-title>
-          </v-card>
+          <h1 class="d-flex justify-center pb-2">You selected</h1>
+          <v-img contain height="300" :src="userChoice.url"/>
         </v-col>
         <v-col cols="12" sm="6">
-          <h1>Computer selected</h1>
-          <v-card>
-            <v-img height="250" :src="computersChoice.url"/>
-            <v-card-title>
-              {{ computersChoice.name }}
-            </v-card-title>
-          </v-card>
+          <h1 class="d-flex justify-center pb-2">Computer selected</h1>
+          <v-img contain height="300" :src="computersChoice.url"/>
         </v-col>
       </v-row>
     </div>
